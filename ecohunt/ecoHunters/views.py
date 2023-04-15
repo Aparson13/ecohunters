f3537110e5c1dc854d932b2058093219cfdaa119
Courtney Nguyen <courtney.nguyen@rain.us>
2023-04-15 12:14:56 -0400

    Ugly Leaderboard

--- ecohunt/ecoHunters/views.py
+++ ecohunt/ecoHunters/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import User
+from operator import attrgetter
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -10,3 +11,11 @@ def home(request):
 
 def signup(request):
     return render(request, 'home.html')
+
+def LeaderBoard(request):
+    users = User.objects.all()
+    sorted_users = sorted(users, key=attrgetter('score'), reverse=True)
+    top_users = sorted_users[:20]
+    context = {'users': top_users}
+    return render(request, 'leaderboard.html', context)
+    
