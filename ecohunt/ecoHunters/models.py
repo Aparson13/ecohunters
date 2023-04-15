3ee085db1b0f83a2821873c9e521466f013993d4
Courtney Nguyen <courtney.nguyen@rain.us>
2023-04-15 11:43:56 -0400

    fixed migrations

--- ecohunt/ecoHunters/models.py
+++ ecohunt/ecoHunters/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=70, blank=True, null=True)
+    username = models.CharField(max_length=70, blank=True, null=True, unique=True)
+
+
