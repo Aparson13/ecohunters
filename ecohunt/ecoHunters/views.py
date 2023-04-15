8200801021bb7c96dc995e2d9d3b76becd3273e3
Katie Tsai <tsaikatie3@gmail.com>
2023-04-15 12:57:06 -0400

    home screen?

--- ecohunt/ecoHunters/views.py
+++ ecohunt/ecoHunters/views.py
@@ -9,4 +9,7 @@ def home(request):
     return render(request, 'home.html')
 
 def signup(request):
-    return render(request, 'home.html')
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
